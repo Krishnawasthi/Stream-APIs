ec533438b95770a84d9a3d740c3d91c06b1e0553
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-04 22:50:29 +0530

    fd

--- Stream_APIs/src/com/collection/StreamAPIs/flatmap/DistinctElement.java
+++ Stream_APIs/src/com/collection/StreamAPIs/flatmap/DistinctElement.java
@@ -1,9 +1,19 @@
 package com.collection.StreamAPIs.flatmap;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DistinctElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		List<Integer>  input = Arrays.asList(12,34,43,7556,354,12,4543,7655,646,674,587,646,675,3453);
+		
+		List<Integer> output = input.stream().distinct().collect(Collectors.toList());
+		
+		System.out.println(output);
+		
 
 	}
 
