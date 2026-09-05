f9494c45071fbcacc820ef581db70cb3fc5d5ad8
Krishnawasthi <km.awasthi.77@gmail.com>
2026-09-05 13:32:09 +0530

    adsd

--- Stream_APIs/src/com/collection/StreamAPIs/problemSolving/FindTheMinimum.java
+++ Stream_APIs/src/com/collection/StreamAPIs/problemSolving/FindTheMinimum.java
@@ -26,21 +26,32 @@ public class FindTheMinimum {
 		
 		System.out.println(minNumber);
 		
-		//Approach5
+        //Approach5
+		Optional<Integer> miniNum = numbers.stream().min((a,b) -> a-b);
+		Optional<Integer> maxNum = numbers.stream().max((a,b) -> a-b);
+		Optional<Integer> maxNumber = numbers.stream().reduce((a, b) -> a+b);
+		System.out.println(miniNum.get());
+		System.out.println(maxNum.get());
+		System.out.println(maxNumber.get());
+		
+		
+		//Approach6
 		Optional<Integer> minimum = numbers.stream().reduce((a,b) -> (a<b) ? a:b);
 		
 		minimum.ifPresent(min -> System.out.println(min));
 		
-		//Approach6
+		//Approach7
 		Optional<Integer> min = numbers.stream().min(Comparator.naturalOrder());
 		System.out.println(min.get());
 		
-		//Appraoch7
+		//Appraoch8
 		Optional<Integer> minNum =  numbers.stream().collect(Collectors.minBy(Comparator.naturalOrder()));
 		System.out.println(minNum.get());
 		
-		//Appraoch8 [int Stream(If working with primitives)]
-		int minIntNum = numbers.stream().mapToInt(Integer :: IntValue).min().getAsInt();
+		//Appraoch9 [int Stream(If working with primitives)]
+		      
+		//  you can replace mapToInt(Integer :: intValue) -->  mapToInt(i -> i.intValue())
+		int minIntNum = numbers.stream().mapToInt(Integer :: intValue).min().getAsInt();
 		System.out.println(minIntNum);
 		
         
